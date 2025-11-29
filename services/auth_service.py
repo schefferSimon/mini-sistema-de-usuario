9d57def69c6ed5778b37666a1006f22e869c22d1
schefferSimon <simonscheffer00@gmail.com>
2025-11-29 05:06:17 -0300

    metodo registrar
    sirve para registrar un nuevo usuario que:
    - no exista previamente
    -la contraseña no se guarde en texto plano
    - la base se actualice automaticamente

--- services/auth_service.py
+++ services/auth_service.py
@@ -34,7 +34,7 @@ class AuthService:
     def guardarUsuarios(self):
 
         """ Guarda los usuarios en el archivo JSON."""
-        
+
         with open(self.DATA_PATH, "w") as f:
             json.dump(self.usuarios, f, indent= 4)
             
@@ -48,3 +48,19 @@ class AuthService:
         """
 
         return hashlib.sha256(password.encode()).hexdigest()
+
+
+    #REGISTRO
+
+    def reguistrar(self,userName, password):
+        """Registra un nuevo usuario si no existe"""
+        
+        if userName in self.usuarios:
+            return False, "El usuario ya existe"
+        
+        passwordHash = self.hashearPassword(password)
+        self.usuarios[userName] = passwordHash
+        self.guardarUsuarios()
+
+        return True, "Usuario registrado correctamente"
+        
