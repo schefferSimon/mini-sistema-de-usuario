0baaddaf447d99a25581e53bb137950b662fa7b3
schefferSimon <simonscheffer00@gmail.com>
2025-11-29 16:45:17 -0300

    interfaz por consola

--- main.py
+++ main.py
@@ -7,3 +7,29 @@ def menu():
     print("3. Salir")
 
 
+def main():
+    autenticacion = AuthService()
+
+    while True:
+        menu()
+        opcion = input("\n seleccione una opcion: ")
+
+        if opcion == "1" :
+            nombre = input("Nuevo usuario: ")
+            contraseña = input("Contraseña: ")
+            ok, msg = autenticacion.reguistrar(nombre, contraseña)
+            print(msg)
+
+        elif opcion == "2":
+            nombre = input("Usuario: ")
+            contraseña = input("Contraseña: ")
+            ok, msg = autenticacion.login(nombre,contraseña)
+            print(msg)
+
+        elif opcion == "3":
+            print("Hasta luego.")
+            break
+
+        else:
+            print("Opcion invalida intente nuevamente")
+        
