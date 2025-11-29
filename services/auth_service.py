944f53cd7f20cbe46f711667ea0647f9c3dbaac0
schefferSimon <simonscheffer00@gmail.com>
2025-11-28 21:33:53 -0300

    metodo cargarUsuario
    lee el archivo json , que contienen los usuarios, si no existe devuelve un diccionario vacio

--- services/auth_service.py
+++ services/auth_service.py
@@ -15,6 +15,17 @@ class AuthService:
     DATA_PATH = "data/usuarios.json"
 
     def __init__(self):
-        self.__usuario = self.cargarUsuarios()
+        self.__usuarios = self.cargarUsuarios()
 
     
+    # CARGA Y GUARDADO
+
+    def cargarUsuarios(self):
+        """ Carga los usuarios desde el archivo JSON."""
+        if not os.path.exists(self.DATA_PATH):
+            return {}
+
+        with open(self.DATA_PATH, "r") as f:
+            return json.load(f)
+
+        
