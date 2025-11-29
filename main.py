ddb79a21fea7e548db63af37c28c0d7aea142b4d
schefferSimon <simonscheffer00@gmail.com>
2025-11-29 16:29:06 -0300

    menu interactivo

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from services.auth_service import AuthService
 
+def menu():
+    print("\n=== MINI SISTEMA DE USUARIOS ===")
+    print("1. Registrar usuario")
+    print("2. Iniciar sesion")
+    print("3. Salir")
+
+
